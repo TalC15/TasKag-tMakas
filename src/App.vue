df85c66792139e95b91b8d46561267d4c5549ec7
TalC15 <talhaakyuz1661@gmail.com>
2024-05-27 18:52:45 +0300

    efsane oyuna meraba

---
+++ src/App.vue
@@ -0,0 +1,120 @@
+<script>
+export default {
+  data() {
+    return {
+      isim: "Mehmet",
+      yas: 26,
+      sayi1: null,
+      sayi2: null,
+    };
+  },
+  methods: {
+    mesajGoster() {
+      const uretilenSayi= (Math.floor(Math.random()*3))+1
+      const uretilenSayi2 = (Math.floor(Math.random()*3))+1
+      this.sayi1 = uretilenSayi;
+      this.sayi2 = uretilenSayi2;
+
+      if(this.sayi1==1 & this.sayi2==2 || this.sayi1==2 & this.sayi2==3 || this.sayi1==3 & this.sayi2==1 ){
+        document.getElementById("kazanan").innerHTML="Kazanan 1.Oyuncu";
+        document.getElementById("kazanan").style.color="tomato";
+      }
+ 
+       else if(this.sayi1 == this.sayi2){
+        document.getElementById("kazanan").innerHTML="BERABERE";
+        document.getElementById("kazanan").style.color="gray";
+      }
+
+      else{
+        document.getElementById("kazanan").innerHTML="Kazanan 2.Oyuncu";
+        document.getElementById("kazanan").style.color="tomato";
+      }
+
+    },
+  },
+};
+</script>
+<template>
+ 
+  <div class="div-1">
+    <div class="div-2">
+      <div class="div-3">
+        <div v-if="sayi1>0" class="oyuncu"><p id="win-1" class="Oyuncu-1">1. Oyuncu</p></div>
+        <div v-if="sayi1 == 1"><img class="obje" src="./assets/images/tas.webp"/></div>
+        <div v-else-if="sayi1 == 2"><img class="obje" src="./assets/images/makas.jpg"/></div>
+        <div v-else-if="sayi1== 3"><img class="obje" src="./assets/images/kagit.webp"/></div>
+        <div v-else></div>
+      </div>
+      <div class="div-3">
+        <div v-if="sayi2>0" class="oyuncu"><p id="win-2" class="Oyuncu-1">2. Oyuncu</p></div>
+        <div  v-if="sayi2 == 1"><img class="obje" src="./assets/images/tas.webp"/></div>
+        <div v-else-if="sayi2 == 2"><img class="obje" src="./assets/images/makas.jpg"/></div>
+        <div v-else-if="sayi2 == 3"><img class="obje" src="./assets/images/kagit.webp"/></div>
+        <div></div>
+      </div>
+    </div>
+  </div>
+  <div>
+    <button id="button" @click="mesajGoster()">tıkla</button>
+  </div>
+    
+  <div id="kazanan">
+
+  </div>
+  
+</template>
+<style>
+.obje{
+  height:120px;
+  width:150px;
+}
+.div-1 {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 500px;
+  margin-bottom:60px;
+}
+.div-2 {
+  margin-bottom: 10px;
+  display:flex;
+  flex-direction: row;
+}
+.div-3{
+  margin-left:430px;
+  font-size:35px;
+}
+#button{
+  margin-left:-410px;
+  width: 750px;
+  font-family: monospace;
+  font-size: 35px;
+  margin-top: 170px;
+  height:54px;
+  border-radius: 7cqb;
+  background-color: lightslategrey;
+  color: aliceblue;
+}
+.Oyuncu-1{
+  margin-top:-63px;
+  color:tomato;
+}
+.oyuncu{
+  width:155px;
+}
+#kazanan{
+  color: #7c7373;
+  width: 70px;
+  height: 205px;
+  margin-top: 23px;
+  margin-left: 369px;
+  font-size: 39px;
+  font-family: auto;
+  font-weight: lighter;
+}
+</style>
+
+
+
+
---
+++ src/elma.vue
@@ -0,0 +1,59 @@
+<script> // JavaScript kodları buraya yazılır.
+export default{ //Varsayılanı dışa aktar demektir.
+data(){
+    // Değişkenler buraya yazılır.
+    return{
+    elma:1,
+    armut:2,
+    inpText: "Mehmet",
+    isimler:["Ali","Mehmet","Cafer","Ahmet"],
+    }
+
+},
+methods:{
+   // fonksiyonlar buraya yazılır.
+   // Burada değişken oluşturmak için değişkenin başına "this." gelir.
+},
+computed:{
+  // fonksiyonlar buraya yazılabilir. Fakat şu üç özelliği var:
+  // 1. Parametre almaz
+  // 2. Parantez almaz
+  // 3. methods'a göre daha kullanışlıdır
+  // Burada değişken oluşturmak için değişkenin başına "this." gelir.
+},
+};
+</script>
+<template><!-- HTML kodları buraya yazılır -->
+
+<!-- v-if: Eğer şart sağlanıyorsa içindeki değer sayfada gözükür. -->
+ <div v-if="elma=1">Merhaba</div>
+
+ <!--v-else-if: Eğer "v-if" deki şart sağlanmazsa bunun içerdiği şarta bakılır, eğer şart sağlanıyorsa sayfada gözükür. -->
+ <div v-else-if="elma=2">Hoşgeldin</div>
+
+ <!-- v-else: Eğer hiçbir şart sağlanmazsa bu çalışır. -->
+ <div v-else>Neden Geldin?</div>
+
+<!-- v-show: "v-if" ile aynı işe yarar ama "v-if" daha güvenlidir. -->
+ <div v-show="armut=2">Görüşürüz</div>
+
+ <!-- @click: "vue" da "onclick" yerine kullanılır. Farklı kullanım şekilleri de vardır. -->
+ <button @click="kek()">Tıklama</button>
+
+ <!-- v-for: Aşağıdaki kek, isimler listesindeki her elemanın karşılığı olur ve hepsini yazdırır. -->
+ <div v-for="kek in isimler">{{ kek }}</div>
+
+ <!-- v-model: Bir yazıyı aynı anda hem input, hem de başka yerde yazdırır. -->
+ <input type="text" v-model="inpText" placeholder="isminizi yazınız" required/>
+ <div>{{ inpText }}</div>
+
+ <!-- Aşağıdaki "value, input" kısmı "v-model" ile aynı görevi yapar. -->
+<input type="text" :value="elma" @input="(e)=>elma=e.target.value" />
+
+<!-- Aşağıdaki kod, rasgele üç sayı oluşturur. -->
+(Math.floor(Math.random()*3))
+
+</template>
+<style> /* CSS kodları buraya yazılır. */
+
+</style>
